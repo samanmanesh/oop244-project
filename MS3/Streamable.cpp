fd5e3098000fe3a6973ce2ddf6edeafc57c99208
Saman <samanmanesh7@gmail.com>
2021-11-22 07:55:25 -0800

    writing virtual method for Streamable interface

--- MS3/Streamable.cpp
+++ MS3/Streamable.cpp
@@ -1,1 +1,24 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
 #include "Streamable.h"
+using namespace sdds;
+using namespace std;
+namespace sdds {
+
+
+
+
+
+	ostream& operator<<(ostream& ostr, const Streamable& RO) {
+	
+		return ostr;
+	};
+
+	istream& operator>>(istream& istr, Streamable& RO) {
+		//RO.readShape(istr);
+		return istr;
+	};
+
+}
--- MS3/Streamable.h
+++ MS3/Streamable.h
@@ -8,20 +8,27 @@ namespace sdds {
 	class Streamable
 	{
 	public:
-		Streamable();
-		~Streamable();
+	
+		virtual ostream& write(ostream& ostr) = 0;
+		
+		virtual istream& read(istream& istr) = 0;
 
 
+		
+		virtual ~Streamable();
 
 	};
 
-	Streamable::Streamable()
-	{
-	}
+	
 
 	Streamable::~Streamable()
 	{
 	}
+
+	std::ostream& operator<<(std::ostream& ostr, const Streamable& RO);
+
+	std::istream& operator>>(std::istream& istr, Streamable& RO);
+
 }
 #endif
 
